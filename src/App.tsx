2d962146059d72c6ccd488b5aa65c4003960dbb8
leobewater <mmbee888@gmail.com>
2023-10-03 14:07:22 -0400

    invalidate query after mutation

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { useQuery, useMutation } from '@tanstack/react-query';
+import { useQuery, useMutation, useQueryClient } from '@tanstack/react-query';
 
 interface IPost {
   id: number | string;
@@ -15,6 +15,8 @@ const wait = (duration: number) => {
 };
 
 function App() {
+  const queryClient = useQueryClient();
+
   const postsQuery = useQuery({
     queryKey: ['posts'],
     queryFn: () => wait(1000).then(() => [...POSTS]),
@@ -23,6 +25,9 @@ function App() {
   const newPostMutation = useMutation({
     mutationFn: (title: string) =>
       wait(1000).then(() => POSTS.push({ id: crypto.randomUUID(), title })),
+    onSuccess: () => {
+      queryClient.invalidateQueries(['posts']);
+    },
   });
 
   if (postsQuery.isLoading) return <h1>Loading...</h1>;
@@ -34,7 +39,9 @@ function App() {
       {postsQuery.data.map((post) => (
         <div key={post.id}>{post.title}</div>
       ))}
-      <button onClick={() => newPostMutation.mutate('New Post')}>Add New</button>
+      <button onClick={() => newPostMutation.mutate('New Post')}>
+        Add New
+      </button>
     </div>
   );
 }
