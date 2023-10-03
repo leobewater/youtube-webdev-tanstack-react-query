8ec7c369e6e64565e4ba6e5fcdb635bae515f42a
leobewater <mmbee888@gmail.com>
2023-10-03 14:05:08 -0400

    mutation with devtools

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,11 @@
 import { useQuery, useMutation } from '@tanstack/react-query';
 
-const POSTS = [
+interface IPost {
+  id: number | string;
+  title: string;
+}
+
+const POSTS: Array<IPost> = [
   { id: 1, title: 'Post 1' },
   { id: 2, title: 'Post 2' },
 ];
@@ -15,14 +20,21 @@ function App() {
     queryFn: () => wait(1000).then(() => [...POSTS]),
   });
 
+  const newPostMutation = useMutation({
+    mutationFn: (title: string) =>
+      wait(1000).then(() => POSTS.push({ id: crypto.randomUUID(), title })),
+  });
+
   if (postsQuery.isLoading) return <h1>Loading...</h1>;
   if (postsQuery.isError) return <pre>{JSON.stringify(postsQuery.error)}</pre>;
 
+  console.log(POSTS);
   return (
     <div>
       {postsQuery.data.map((post) => (
         <div key={post.id}>{post.title}</div>
       ))}
+      <button onClick={() => newPostMutation.mutate('New Post')}>Add New</button>
     </div>
   );
 }
