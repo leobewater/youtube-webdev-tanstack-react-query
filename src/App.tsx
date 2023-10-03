0ec94a360cf0e23c18d062bec14017098a29ef1e
leobewater <mmbee888@gmail.com>
2023-10-03 13:59:09 -0400

    fetch data

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,30 @@
-import './App.css'
+import { useQuery, useMutation } from '@tanstack/react-query';
+
+const POSTS = [
+  { id: 1, title: 'Post 1' },
+  { id: 2, title: 'Post 2' },
+];
+
+const wait = (duration: number) => {
+  return new Promise((resolve) => setTimeout(resolve, duration));
+};
 
 function App() {
+  const postsQuery = useQuery({
+    queryKey: ['posts'],
+    queryFn: () => wait(1000).then(() => [...POSTS]),
+  });
+
+  if (postsQuery.isLoading) return <h1>Loading...</h1>;
+  if (postsQuery.isError) return <pre>{JSON.stringify(postsQuery.error)}</pre>;
 
   return (
-    <h1>TanStack Query</h1>
-  )
+    <div>
+      {postsQuery.data.map((post) => (
+        <div key={post.id}>{post.title}</div>
+      ))}
+    </div>
+  );
 }
 
-export default App
+export default App;
